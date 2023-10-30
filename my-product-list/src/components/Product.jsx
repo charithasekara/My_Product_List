7f54dc7ba885921d5b882eaf98b5e9a67c5ef06a
charithasekara <86426242+charithasekara@users.noreply.github.com>
2023-10-30 08:48:58 +0530

    photo swap changed

--- my-product-list/src/components/Product.jsx
+++ my-product-list/src/components/Product.jsx
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from 'react';
+import productData from '../data/products.json';
 import '../App.css';
 
 const Product = ({ product }) => {
   const [imageIndex, setImageIndex] = useState(0);
+  const [isHovered, setIsHovered] = useState(false);
 
   useEffect(() => {
     const updateImageIndex = () => {
-      setImageIndex((prevIndex) => (prevIndex + 1) % product.images.length);
+      if (isHovered) {
+        // Change the image only when hovered
+        setImageIndex((prevIndex) => (prevIndex + 1) % product.images.length);
+      }
     };
 
     const intervalId = setInterval(updateImageIndex, 3000);
@@ -14,10 +19,14 @@ const Product = ({ product }) => {
     return () => {
       clearInterval(intervalId);
     };
-  }, [product]);
+  }, [product, isHovered]);
 
   return (
-    <div className="product">
+    <div
+      className={`product ${isHovered ? 'hovered' : ''}`}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <img src={`/assets/${product.images[imageIndex]}`} alt={product.name} />
       <h3>{product.name}</h3>
       <p>${product.price}</p>
@@ -27,7 +36,6 @@ const Product = ({ product }) => {
         <button>Card</button>
         <button>Credit</button>
       </div>
-      
     </div>
   );
 };
